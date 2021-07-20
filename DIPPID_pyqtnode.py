efa99410cfe98946b83cae0f6eff6c3d3ccaa960
michischmidt <schmidt.m.dev@gmail.com>
2021-07-20 18:07:04 +0200

    Added axis signal representation

--- DIPPID_pyqtnode.py
+++ DIPPID_pyqtnode.py
@@ -10,6 +10,7 @@ import pyqtgraph as pg
 import numpy as np
 from DIPPID import SensorUDP, SensorSerial, SensorWiimote
 import sys
+from drumkit import FftNode
 
 
 class BufferNode(Node):
@@ -32,9 +33,11 @@ class BufferNode(Node):
         Node.__init__(self, name, terminals=terminals)
 
     def process(self, **kwds):
-        self._buffer = np.append(self._buffer, kwds['dataIn'])[-self.buffer_size:]
+        self._buffer = np.append(
+            self._buffer, kwds['dataIn'])[-self.buffer_size:]
 
         return {'dataOut': self._buffer}
+
 
 fclib.registerNodeType(BufferNode, [('Data',)])
 
@@ -123,7 +126,7 @@ class DIPPIDNode(Node):
         address = self.text.text().strip()
         self.connect_button.setText("connecting...")
 
-        if '/dev/tty' in address: # serial tty
+        if '/dev/tty' in address:  # serial tty
             self.dippid = SensorSerial(address)
         elif ':' in address:
             self.dippid = SensorWiimote(address)
@@ -155,7 +158,82 @@ class DIPPIDNode(Node):
     def process(self, **kwdargs):
         return {'accelX': np.array([self._acc_vals[0]]), 'accelY': np.array([self._acc_vals[1]]), 'accelZ': np.array([self._acc_vals[2]])}
 
+
 fclib.registerNodeType(DIPPIDNode, [('Sensor',)])
+fclib.registerNodeType(FftNode, [("FftNode",)])
+
+
+def xPlot(fc, fftNode, dippidNode):
+    pw1 = pg.PlotWidget()
+    layout.addWidget(pw1, 0, 1)
+    pw1.setYRange(0, 1)
+
+    pw1Node = fc.createNode('PlotWidget', pos=(0, -150))
+    pw1Node.setPlot(pw1)
+
+    bufferNodeX = fc.createNode('Buffer', pos=(150, 0))
+
+    fc.connectTerminals(dippidNode['accelX'], bufferNodeX['dataIn'])
+    fc.connectTerminals(bufferNodeX['dataOut'], pw1Node['In'])
+
+    pw2 = pg.PlotWidget()
+    layout.addWidget(pw2, 0, 2)
+    pw2.setYRange(0, 1)
+
+    pw2Node = fc.createNode('PlotWidget', pos=(0, -300))
+    pw2Node.setPlot(pw2)
+
+    fc.connectTerminals(bufferNodeX['dataOut'], fftNode['accelX'])
+    fc.connectTerminals(fftNode['frequencyX'], pw2Node['In'])
+
+
+def yPlot(fc, fftNode, dippidNode):
+    pw1 = pg.PlotWidget()
+    layout.addWidget(pw1, 1, 1)
+    pw1.setYRange(0, 1)
+
+    pw1Node = fc.createNode('PlotWidget', pos=(0, -150))
+    pw1Node.setPlot(pw1)
+
+    bufferNodeY = fc.createNode('Buffer', pos=(150, 0))
+
+    fc.connectTerminals(dippidNode['accelY'], bufferNodeY['dataIn'])
+    fc.connectTerminals(bufferNodeY['dataOut'], pw1Node['In'])
+
+    pw2 = pg.PlotWidget()
+    layout.addWidget(pw2, 1, 2)
+    pw2.setYRange(0, 1)
+
+    pw2Node = fc.createNode('PlotWidget', pos=(0, -300))
+    pw2Node.setPlot(pw2)
+
+    fc.connectTerminals(bufferNodeY['dataOut'], fftNode['accelY'])
+    fc.connectTerminals(fftNode['frequencyY'], pw2Node['In'])
+
+
+def zPlot(fc, fftNode, dippidNode):
+    pw1 = pg.PlotWidget()
+    layout.addWidget(pw1, 2, 1)
+    pw1.setYRange(0, 1)
+
+    pw1Node = fc.createNode('PlotWidget', pos=(0, -150))
+    pw1Node.setPlot(pw1)
+
+    bufferNodeZ = fc.createNode('Buffer', pos=(150, 0))
+
+    fc.connectTerminals(dippidNode['accelZ'], bufferNodeZ['dataIn'])
+    fc.connectTerminals(bufferNodeZ['dataOut'], pw1Node['In'])
+
+    pw2 = pg.PlotWidget()
+    layout.addWidget(pw2, 2, 2)
+    pw2.setYRange(0, 1)
+
+    pw2Node = fc.createNode('PlotWidget', pos=(0, -300))
+    pw2Node.setPlot(pw2)
+
+    fc.connectTerminals(bufferNodeZ['dataOut'], fftNode['accelZ'])
+    fc.connectTerminals(fftNode['frequencyZ'], pw2Node['In'])
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication([])
@@ -171,19 +249,11 @@ if __name__ == '__main__':
     w = fc.widget()
 
     layout.addWidget(fc.widget(), 0, 0, 2, 1)
-
-    pw1 = pg.PlotWidget()
-    layout.addWidget(pw1, 0, 1)
-    pw1.setYRange(0, 1)
-
-    pw1Node = fc.createNode('PlotWidget', pos=(0, -150))
-    pw1Node.setPlot(pw1)
-
     dippidNode = fc.createNode("DIPPID", pos=(0, 0))
-    bufferNode = fc.createNode('Buffer', pos=(150, 0))
-
-    fc.connectTerminals(dippidNode['accelX'], bufferNode['dataIn'])
-    fc.connectTerminals(bufferNode['dataOut'], pw1Node['In'])
+    fftNode = fc.createNode("FftNode", pos=(0, 0))
+    xPlot(fc, fftNode, dippidNode)
+    yPlot(fc, fftNode, dippidNode)
+    zPlot(fc, fftNode, dippidNode)
 
     win.show()
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
